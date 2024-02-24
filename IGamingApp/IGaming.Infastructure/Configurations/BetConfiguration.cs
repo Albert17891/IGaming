697a9722a29b4e61d81bae12bfafa43343b65f5a
Gevorkyan Albert <abogevorgian@gmail.com>
2024-02-24 11:47:45 +0400

    add configuration for entities

---
+++ IGamingApp/IGaming.Infastructure/Configurations/BetConfiguration.cs
@@ -0,0 +1,23 @@
+﻿using IGaming.Domain.Models;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+
+namespace IGaming.Infrastructure.Configurations;
+
+public class BetConfiguration : IEntityTypeConfiguration<Bet>
+{
+    public void Configure(EntityTypeBuilder<Bet> builder)
+    {
+        builder.HasKey(x => x.Id);
+
+        builder.Property(x => x.Details).IsRequired();
+
+        builder.Property(x => x.Amount)
+            .IsRequired()
+            .HasDefaultValue(1000);
+
+        builder.HasOne(b => b.User)
+               .WithMany(u => u.Bets)
+               .HasForeignKey(b => b.UserId);
+    }
+}
