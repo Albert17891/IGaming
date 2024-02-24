91ccf9854f996f478c64178f3a3643141247d5ba
Gevorkyan Albert <abogevorgian@gmail.com>
2024-02-24 15:09:54 +0400

    add new Models and DataAnnotations

--- IGamingApp/IGaming/Models/Request/RegisterRequest.cs
+++ IGamingApp/IGaming/Models/Request/RegisterRequest.cs
@@ -1,9 +1,18 @@
-﻿namespace IGaming.Models.Request;
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace IGaming.Models.Request;
 
 public class RegisterRequest
 {
+    [Required]
     public string UserName { get; set; }
+    [Required]
+    [EmailAddress(ErrorMessage = "value is not valid email address")]
     public string Email { get; set; }
+    [Required]
+    [MinLength(6, ErrorMessage = "Minimal Password length must be 6")]
     public string Password { get; set; }
+    [Required]
+    [MinLength(6, ErrorMessage = "Minimal Password length must be 6")]
     public string ConfirmPassword { get; set; }
 }
---
+++ IGamingApp/IGaming/Models/Response/UserResponse.cs
@@ -0,0 +1,7 @@
+﻿namespace IGaming.Models.Response;
+
+public class UserResponse
+{
+    public string UserName { get; set; }
+    public string Email { get; set; }  
+}
