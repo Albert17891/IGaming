d2bd6a1d5ffb29b4d5b2368a1aec85220eb17976
Gevorkyan Albert <abogevorgian@gmail.com>
2024-02-24 11:45:13 +0400

    add services and configuration

--- IGamingApp/IGaming/Program.cs
+++ IGamingApp/IGaming/Program.cs
@@ -1,3 +1,7 @@
+using Asp.Versioning;
+using IGaming.Core;
+using IGaming.Infrastructure;
+
 namespace IGaming;
 
 public class Program
@@ -6,12 +10,30 @@ public class Program
     {
         var builder = WebApplication.CreateBuilder(args);
 
+        var configuration = builder.Configuration;
         // Add services to the container.
 
         builder.Services.AddControllers();
         // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
         builder.Services.AddEndpointsApiExplorer();
         builder.Services.AddSwaggerGen();
+
+        builder.Services.AddInfrastructureServices(configuration);
+        builder.Services.AddCoreServices(configuration);
+
+        builder.Services.AddApiVersioning(options =>
+        {
+            options.DefaultApiVersion = new ApiVersion(1);
+            options.ReportApiVersions = true;
+            options.AssumeDefaultVersionWhenUnspecified = true;
+            options.ApiVersionReader = ApiVersionReader.Combine(
+                new UrlSegmentApiVersionReader(),
+                new HeaderApiVersionReader("X-Api-Version"));
+        }).AddApiExplorer(options =>
+        {
+            options.GroupNameFormat = "'v'V";
+            options.SubstituteApiVersionInUrl = true;
+        });
 
         var app = builder.Build();
 
