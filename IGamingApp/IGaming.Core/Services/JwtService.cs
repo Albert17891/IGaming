7eae5dd8c7315a80644043f7c23717a7db34e623
Gevorkyan Albert <abogevorgian@gmail.com>
2024-02-24 11:45:41 +0400

    implement it

---
+++ IGamingApp/IGaming.Core/Services/JwtService.cs
@@ -0,0 +1,46 @@
+﻿using IGaming.Core.Interfaces;
+using IGaming.Domain.Options;
+using Microsoft.Extensions.Options;
+using Microsoft.IdentityModel.Tokens;
+using System.IdentityModel.Tokens.Jwt;
+using System.Security.Claims;
+using System.Text;
+
+namespace Market.Application.Services;
+
+public class JwtService : IJwtService
+{
+    private readonly AccessTokenConfiguration _options;
+
+    public JwtService(IOptions<AccessTokenConfiguration> options)
+    {
+        _options = options.Value;
+    }
+    public string GetJwtToken(string userName)
+    {
+        var issuer = _options.Issuer;
+        var audience = _options.Audience;
+        var key = Encoding.UTF8.GetBytes(_options.Key);
+        var expiresMinutes = _options.AccessTokenExpiresMinutes;
+
+        var securityTokenDescriptor = new SecurityTokenDescriptor()
+        {
+            Subject = new ClaimsIdentity(new[]
+            {
+                new Claim("Id", Guid.NewGuid().ToString()),
+                new Claim(JwtRegisteredClaimNames.UniqueName, userName),
+            }),
+
+            Expires = DateTime.Now.AddMinutes(expiresMinutes),
+            Issuer = issuer,
+            Audience = audience,
+            SigningCredentials = new SigningCredentials(new SymmetricSecurityKey(key), SecurityAlgorithms.HmacSha256),
+        };
+
+        var tokenHandler = new JwtSecurityTokenHandler();
+        var token = tokenHandler.CreateToken(securityTokenDescriptor);
+        var accessToken = tokenHandler.WriteToken(token);
+
+        return accessToken;
+    }
+}
---
+++ IGamingApp/IGaming.Core/Services/UserService.cs
@@ -0,0 +1,79 @@
+﻿using IGaming.Core.Interfaces;
+using IGaming.Core.Models;
+using IGaming.Domain.Exceptions;
+using IGaming.Domain.Models;
+using IGaming.Infrastructure.Interfaces;
+using Mapster;
+using Microsoft.EntityFrameworkCore;
+using System.Security.Cryptography;
+using System.Text;
+
+namespace IGaming.Core.Services;
+public class UserService : IUserService
+{
+    private readonly IUnitOfWork _unitOfWork;
+    private readonly IJwtService _jwtService;
+    private const int Iterations = 10000;
+    private const int SaltSize = 16;
+    private const int HashSize = 20;
+
+    public UserService(IUnitOfWork unitOfWork, IJwtService jwtService)
+    {
+        _unitOfWork = unitOfWork;
+        _jwtService = jwtService;
+    }
+    public async Task<string> Login(string username, string password)
+    {
+        var user = await _unitOfWork.Repository<User>().Table
+                                              .SingleOrDefaultAsync(x => x.UserName == username);
+
+        if (user != null && VerifyPassword(password, user.Password))
+        {
+            return _jwtService.GetJwtToken(username);
+        }
+
+        throw new UserNotFoundException("User Not Found");
+    }
+
+    public async Task Register(UserServiceModel userServiceModel, CancellationToken cancellationToken)
+    {
+        if (userServiceModel.Password != userServiceModel.ConfirmPassword)
+        {
+            throw new ArgumentException("ConfirmPassword must be equal to Password");
+        }
+
+
+        var isExist = await _unitOfWork.Repository<User>().Table.AnyAsync(x => x.UserName == userServiceModel.UserName, cancellationToken);
+
+        if (isExist)
+        {
+            throw new SameUserNameExceptions($"The username  {userServiceModel.UserName} exist,Use Another Name");
+        }
+
+
+
+        var user = userServiceModel.Adapt<User>();
+
+        user.Password = HashPassword(userServiceModel.Password);
+
+        await _unitOfWork.Repository<User>().AddAsync(user, cancellationToken);
+
+        await _unitOfWork.SaveChangeAsync();
+    }
+
+    private string HashPassword(string password)
+    {
+        using SHA256 sha256 = SHA256.Create();
+
+        byte[] hashedBytes = sha256.ComputeHash(Encoding.UTF8.GetBytes(password));
+
+        return Convert.ToBase64String(hashedBytes);
+    }
+
+    private bool VerifyPassword(string enteredPassword, string storedHashedPassword)
+    {
+        string enteredHash = HashPassword(enteredPassword);
+
+        return storedHashedPassword.Equals(enteredHash);
+    }
+}
