654ff88a1db7b58ebca1c38c6275760bb6e789a5
Gevorkyan Albert <abogevorgian@gmail.com>
2024-02-24 11:47:21 +0400

    implement it

---
+++ IGamingApp/IGaming.Infastructure/BaseRepository.cs
@@ -0,0 +1,40 @@
+﻿using IGaming.Infrastructure.Interfaces;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace IGaming.Infrastructure;
+public class BaseRepository<T> : IBaseRepository<T> where T : class
+{
+    private readonly AppDbContext _context;
+    private readonly DbSet<T> _dbSet;
+
+    public BaseRepository(AppDbContext context)
+    {
+        _context = context;
+        _dbSet = _context.Set<T>();
+    }
+
+    public IQueryable<T> Table
+    {
+        get { return _dbSet; }
+    }
+
+    public async Task AddAsync(T entity, CancellationToken cancellationToken)
+    {
+        await _context.AddAsync(entity, cancellationToken);
+    }
+
+    public void Delete(T entity)
+    {
+        _context.Remove(entity);
+    }
+
+    public void Update(T entity)
+    {
+        _context.Update(entity);
+    }
+}
---
+++ IGamingApp/IGaming.Infastructure/UnitOfWork.cs
@@ -0,0 +1,41 @@
+﻿using IGaming.Infrastructure.Interfaces;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace IGaming.Infrastructure;
+public class UnitOfWork : IUnitOfWork
+{
+    private readonly AppDbContext _context;
+    private Dictionary<Type, object> _repositories;
+
+    public UnitOfWork(AppDbContext context)
+    {
+        _context = context;
+    }
+
+    public IBaseRepository<T> Repository<T>() where T : class
+    {
+        if (_repositories is null)
+        {
+            _repositories = new Dictionary<Type, object>();
+        }
+
+        var type = typeof(T);
+        if (!_repositories.ContainsKey(type))
+        {
+            var repositoryType = typeof(BaseRepository<>);
+            var repositoryInstance = Activator.CreateInstance(repositoryType.MakeGenericType(type), _context);
+            _repositories[type] = repositoryInstance;
+        }
+
+        return (IBaseRepository<T>)_repositories[type];
+    }
+
+    public async Task<int> SaveChangeAsync()
+    {
+        return await _context.SaveChangesAsync();
+    }
+}
