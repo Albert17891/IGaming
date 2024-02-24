c139f2e0cf28d1a4153f7ff31b953f5c7aff6920
Gevorkyan Albert <abogevorgian@gmail.com>
2024-02-24 15:10:07 +0400

    add new BetController

---
+++ IGamingApp/IGaming/Controllers/v1/BetController.cs
@@ -0,0 +1,43 @@
+﻿using Asp.Versioning;
+using IGaming.Core.Interfaces;
+using IGaming.Core.Models;
+using IGaming.Models.Request;
+using Mapster;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+
+namespace IGaming.Controllers.v1;
+
+
+[ApiVersion(1)]
+[Authorize]
+[Route("api/v{v:apiVersion}/bets")]
+[ApiController]
+public class BetController : ControllerBase
+{
+    private readonly IBetService _betService;
+
+    public BetController(IBetService betService)
+    {
+        _betService = betService;
+    }
+
+    [MapToApiVersion(1)]    
+    [Route("profile")]
+    [HttpPost]
+    public async Task<IActionResult> PlaceBet(BetRequest betRequest, CancellationToken cancellationToken = default)
+    {
+        var token = HttpContext.Request.Headers["Authorization"].FirstOrDefault()?.Split(" ").Last();
+
+        if (token == null)
+        {
+            return BadRequest();
+        }
+
+        var betServiceModel = betRequest.Adapt<BetServiceModel>();
+
+        await _betService.CreateBet(betServiceModel,token, cancellationToken);
+
+        return Ok();
+    }
+}
