018d59ab3c9f8bf2472c46377dd5a48f1fdaf906
Gevorkyan Albert <abogevorgian@gmail.com>
2024-02-24 11:47:07 +0400

    add infrastructure interfaces

---
+++ IGamingApp/IGaming.Infastructure/Interfaces/IBaseRepository.cs
@@ -0,0 +1,8 @@
+﻿namespace IGaming.Infrastructure.Interfaces;
+public interface IBaseRepository<T> where T : class
+{
+    IQueryable<T> Table { get; }
+    Task AddAsync(T entity, CancellationToken cancellationToken);
+    void Update(T entity);
+    void Delete(T entity);
+}
---
+++ IGamingApp/IGaming.Infastructure/Interfaces/IUnitOfWork.cs
@@ -0,0 +1,7 @@
+﻿namespace IGaming.Infrastructure.Interfaces;
+
+public interface IUnitOfWork
+{
+    IBaseRepository<T> Repository<T>() where T : class;
+    Task<int> SaveChangeAsync();
+}
